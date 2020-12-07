611643085425661dd8f5789619eef0622ed2defc
MinyShrimp <ksk7584@gmail.com>
2020-12-07 21:46:54 +0900

    first push

---
+++ web/containers/First.js
@@ -0,0 +1,10 @@
+// First.js
+import React, { Component } from 'react'
+
+export default class First extends Component {
+    render() {
+        return (
+            <div>Hello First</div>
+        )
+    }
+}
---
+++ web/containers/Second.js
@@ -0,0 +1,10 @@
+// Second.js
+import React, { Component } from 'react'
+
+export default class Second extends Component {
+    render() {
+        return (
+            <div>Hello Second</div>
+        )
+    }
+}
